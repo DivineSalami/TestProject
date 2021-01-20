ad6d3966230dd49617cc79daab860a285f533857
p.m.student <Bolden.Chance02@gmail.com>
2021-01-20 13:24:34 -0600

    Initial Commit

---
+++ TestProject2/TestProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  TestProject2App.swift
+//  TestProject2
+//
+//  Created by P.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct TestProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestProject2Tests/TestProject2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  TestProject2Tests.swift
+//  TestProject2Tests
+//
+//  Created by P.M. Student on 1/20/21.
+//
+
+import XCTest
+@testable import TestProject2
+
+class TestProject2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
